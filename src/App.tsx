65a7fc5311c6006b667487dc56db56969be6e825
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-03-10 14:30:00 +0100

    added state to expand and hide folder files

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,66 @@
 import "./App.css";
+import {useState} from 'react'
 
-function App() {
-  const files = {
-    children: [
-      {
-        name: "public",
-        children: [
-          {
-            name:"logo.svg"
-          }
-        ],
-      },
-      {
-        name:"package.json"
-      },
-      {
-        name:"tsconfig.json"
-      }
 
-    ],
-  };
-  type TEntry = {
-
-  }
-  function Entry({name,children}:{name:string,children?: []}){
-    return
-      
-      
-  }
-  return <div className="App">
+const files = {
+  children: [
     {
-      files.map(entry =><Entry {..entry}/>
-      
-      )
-    }
-  </div>;
+      name: "Public",
+      children: [
+        {
+          name:"logo.svg",
+        },
+        {
+          name:"Images",
+          children: [
+            {
+              name:"profile.jpeg",
+            },
+            {
+              name:"hero.png",
+            }
+          ]
+        }
+      ],
+    },
+    {
+      name:"package.json"
+    },
+    {
+      name:"tsconfig.json"
+    },
+   
+  ],
+};
+type TEntry = {
+  name:string;
+  children?: TEntry[];
+}
+function Entry({entry ,depth}:{entry:TEntry;depth:number}){
+  const [open,setOpen]=useState(true);
+  return (
+  <div className="folder">
+    <div onClick={()=>setOpen(!open)}>
+    {entry.children && (open?'- ':'+ ')}
+    {' '+entry.name}
+    </div>
+    <div style={{paddingLeft:`${depth*10}px`}}>
+     {open && entry.children?.map((entry) =>
+        <Entry entry={entry} depth={depth+1}/> )
+        }
+    </div>
+  </div>
+  ); 
+}
+function App() {
+  
+  return (
+  <div className="App">
+    <p>File Explorer</p>
+  {files.children.map((entry) =>(
+       <Entry entry={entry} depth={1}/> 
+      ))}
+</div>);
 }
 
 export default App;
